e88cd79a0550aac52fb6674b2aa9863099742fbe
cahenk <cahenk95@gmail.com>
2017-10-16 12:33:38 -0500

    Project skeleton based on activity diagram

---
+++ app/src/main/java/com/dashi/android/dashidashcam/AccountActivity.java
@@ -0,0 +1,16 @@
+package com.dashi.android.dashidashcam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+
+}
+
